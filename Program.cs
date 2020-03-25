7ced86a629e8847632929bf6df74aafe2ccc6686
kid2201 <48373691+kid2201@users.noreply.github.com>
2020-03-25 23:26:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _122160022_DangHuynhKhanhLy_16PFIEV3.GUI;
+namespace _122160022_DangHuynhKhanhLy_16PFIEV3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
